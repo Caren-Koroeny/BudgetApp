2b8e2eff3e32c5ae0e54634030f8de4fa443d4b7
Caren-Koroeny <carensiya5@gmail.com>
2023-07-13 18:04:39 +0300

    Category model added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_and_belongs_to_many :products
+  
+    validates :name, presence: true
+    validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
